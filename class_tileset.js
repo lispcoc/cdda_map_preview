b3e19d3fb76b56017fab7b259964664f14ff5510
lispcoc <fourier0520@gmail.com>
2023-07-25 13:36:48 +0900

    multitile

--- class_tileset.js
+++ class_tileset.js
@@ -9,7 +9,7 @@ class Tileset {
         this.id_cache = {}
         this.valid = false
         this.onload_events = onload_events
-        this.on_subset_load = (subset) => {
+        this.on_subset_load = () => {
             for(var s of this.sub){
                 if(!s.valid) {
                     return false
@@ -38,7 +38,7 @@ class Tileset {
         for(var subset of this.sub) {
             for(var tile of subset.tiles) {
                 force_array(tile.id).forEach(id => {
-                    this.tiles[id] = new TileData(tile, this.sprites)
+                    this.tiles[id] = new TileData(id, tile, this.sprites)
                 })
             }
         }
@@ -46,25 +46,70 @@ class Tileset {
     is_valid() {
         return this.valid
     }
-    draw(canvas_ctx, id, x, y){
+    draw_sprite(canvas_ctx, index, x, y){
+        const sprite = this.sprites[index]
+        canvas_ctx.drawImage(
+            sprite.img,
+            sprite.start_x, sprite.start_y,
+            sprite.width, sprite.height,
+            x * this.width + sprite.offset_x, y * this.height + sprite.offset_y,
+            sprite.width, sprite.height
+        )
+        
+    }
+    draw_all(canvas_ctx, a = [[]], offset_x = 0, offset_y = 0){
+        const compare_safe = (a, y1, x1, y2, x2, target = "fg") => {
+            if(!Array.isArray(a[y1])){
+                return false
+            }
+            if(x1 > a[y1].length || x1 < 0 || !this.tiles[a[y1][x1]] || !Array.isArray(this.tiles[a[y1][x1]][target])){
+                return false
+            }
+            if(!Array.isArray(a[y2])){
+                return false
+            }
+            if(x2 > a[y2].length || x2 < 0 || !this.tiles[a[y2][x2]] || !Array.isArray(this.tiles[a[y2][x2]][target])){
+                return false
+            }
+            return this.tiles[a[y1][x1]][target][0] == this.tiles[a[y2][x2]][target][0]
+        }
+
+        for(var y = 0; y < a.length; y++) {
+            for(var x = 0; x < a[y].length; x++) {
+                var connect = DIR_NO
+                if(compare_safe(a, y, x, y - 1, x)) {connect |= DIR_U}
+                if(compare_safe(a, y, x, y, x - 1)) {connect |= DIR_L}
+                if(compare_safe(a, y, x, y + 1, x)) {connect |= DIR_D}
+                if(compare_safe(a, y, x, y, x + 1)) {connect |= DIR_R}
+                if(compare_safe(a, y, x, y - 1, x - 1)) {connect |= DIR_UL}
+                if(compare_safe(a, y, x, y + 1, x - 1)) {connect |= DIR_DL}
+                if(compare_safe(a, y, x, y + 1, x + 1)) {connect |= DIR_DR}
+                if(compare_safe(a, y, x, y - 1, x + 1)) {connect |= DIR_UR}
+                this.draw(canvas_ctx, a[y][x], x + offset_x, y + offset_y, connect)
+            }
+        }
+    }
+    draw(canvas_ctx, id, x, y, connect = DIR_NO){
         const tile = this.tiles[id]
         if(tile){
-            if(tile.bg){
+            const fg = tile.connected_sprite("fg", connect)
+            const bg = tile.connected_sprite("bg", connect)
+            if(bg){
                 canvas_ctx.drawImage(
-                  tile.bg.img,
-                  tile.bg.start_x, tile.bg.start_y,
-                  tile.bg.width, tile.bg.height,
-                  x * this.width + tile.bg.offset_x, y * this.height + tile.bg.offset_y,
-                  tile.bg.width, tile.bg.height
+                  bg.img,
+                  bg.start_x, bg.start_y,
+                  bg.width, bg.height,
+                  x * this.width + bg.offset_x, y * this.height + bg.offset_y,
+                  bg.width, bg.height
                 )
             }
-            if(tile.fg){
+            if(fg){
                 canvas_ctx.drawImage(
-                  tile.fg.img,
-                  tile.fg.start_x, tile.fg.start_y,
-                  tile.fg.width, tile.fg.height,
-                  x * this.width + tile.fg.offset_x, y * this.height + tile.fg.offset_y,
-                  tile.fg.width, tile.fg.height
+                  fg.img,
+                  fg.start_x, fg.start_y,
+                  fg.width, fg.height,
+                  x * this.width + fg.offset_x, y * this.height + fg.offset_y,
+                  fg.width, fg.height
                 )
             }
         }
@@ -101,48 +146,85 @@ class Subset {
         this.ysize = parseInt(this.img.height / this.sprite_height)
         this.size = this.xsize * this.ysize
     }
-    get_id(id_str) {
-        for(var t of this.tiles) {
-            if(Array.isArray(t.id) && t.id.find(id => id == id_str) || t.id == id_str){
-                return {fg: this.get_fgbg(t.fg), bg: this.get_fgbg(t.bg)}
-            }
-        }
-        return null
-    }
-    get_fgbg(t) {
-        var fgbg = get_first(t)
-        if(isObject(fgbg)){
-            return fgbg.sprite
-        }
-        return fgbg
-    }
 }
 
 class TileData {
-    constructor(json, sprites) {
-        this.fg = sprites[this.get_fgbg(json.fg)]
-        this.bg = sprites[this.get_fgbg(json.bg)]
-    }
-
-    get_fgbg(fgbg) {
+    constructor(id, json, sprites) {
         const force_array = (fgbg) => {
             if(Array.isArray(fgbg)){
                 return fgbg
             }
             return [fgbg]
         }
-
-        var e = force_array(fgbg)[0]
-        if(isObject(e)){
-            return e.sprite
+        const get_num = (fgbg) => {
+            var e = force_array(fgbg)[0]
+            if(isObject(e)){
+                return e.sprite
+            }
+            return e
         }
-        return e
+
+        this.id = id
+        this.fg = force_array(json.fg).map(fg => sprites[get_num(fg)])
+        this.bg = force_array(json.bg).map(bg => sprites[get_num(bg)])
+
+        if(json.additional_tiles){
+            this.additional_tiles = {}
+            json.additional_tiles.forEach(json => {
+                this.additional_tiles[json.id] = new TileData(json.id, json, sprites)
+            })
+        }
+
+        const sample = {
+          "id": "t_ice",
+          "fg": 1949,
+          "multitile": true,
+          "additional_tiles": [
+            { "id": "center", "fg": 1934 },
+            { "id": "corner", "fg": [ 1936, 1938, 1937, 1935 ], "//": "[左上, 左下, 右下, 右上]"}, 
+            { "id": "t_connection", "fg": [ 1946, 1948, 1947, 1945 ], "//": "[上淵, 左淵, 下淵, 右淵]" },
+            { "id": "edge", "fg": [ 1940, 1939 ], "//": "[垂直, 水平]" },
+            { "id": "end_piece", "fg": [ 1942, 1944, 1943, 1941 ], "//": "[上終端, 左終端, 下終端, 右終端]" },
+            { "id": "unconnected", "fg": [ 1949, 1949 ] }
+          ]
+        }
+    }
+    connected_sprite(target = "fg", connected = DIR_NO){
+        const set = [
+            { cond: CENTER, target: "center", index: 0},
+            { cond: TC_U, target: "t_connection", index: 0},
+            { cond: TC_L, target: "t_connection", index: 1},
+            { cond: TC_D, target: "t_connection", index: 2},
+            { cond: TC_R, target: "t_connection", index: 3},
+            { cond: CORNER_UL, target: "corner", index: 0},
+            { cond: CORNER_DL, target: "corner", index: 1},
+            { cond: CORNER_DR, target: "corner", index: 2},
+            { cond: CORNER_UR, target: "corner", index: 3},
+            { cond: EDGE_V, target: "edge", index: 0},
+            { cond: EDGE_H, target: "edge", index: 1},
+            { cond: END_U, target: "end_piece", index: 0},
+            { cond: END_L, target: "end_piece", index: 1},
+            { cond: END_D, target: "end_piece", index: 2},
+            { cond: END_R, target: "end_piece", index: 3},
+        ]
+        if(this.additional_tiles){
+            var e = set.find(e => {return (e.cond & connected) == e.cond})
+            if(e && this.additional_tiles[e.target]) {
+                if(this.additional_tiles[e.target][target]){
+                    if(this.additional_tiles[e.target][target][e.index]){
+                        return this.additional_tiles[e.target][target][e.index]
+                    }
+                }
+            }
+        }
+        return this[target][0]
     }
 }
 
 class SpriteData {
     constructor(subset, index) {
         this.img = subset.img
+        this.index = index
         this.start_x = (index % subset.xsize) * subset.sprite_width
         this.start_y = parseInt(index / subset.xsize) * subset.sprite_height
         this.width = subset.sprite_width
--- common.js
+++ common.js
@@ -9,3 +9,27 @@ const isObject = (value) => {
     return value !== null && typeof value === 'object'
 }
 
+const DIR_UL = 0b10000000
+const DIR_U  = 0b01000000
+const DIR_UR = 0b00100000
+const DIR_L  = 0b00010000
+const DIR_R  = 0b00001000
+const DIR_DL = 0b00000100
+const DIR_D  = 0b00000010
+const DIR_DR = 0b00000001
+const DIR_NO = 0b00000000
+const CENTER = DIR_U | DIR_D | DIR_R | DIR_L
+const TC_U = DIR_L | DIR_D | DIR_R
+const TC_L = DIR_U | DIR_D | DIR_R
+const TC_D = DIR_U | DIR_L | DIR_R
+const TC_R = DIR_U | DIR_L | DIR_D
+const CORNER_UL = DIR_R | DIR_D
+const CORNER_UR = DIR_L | DIR_D
+const CORNER_DL = DIR_R | DIR_U
+const CORNER_DR = DIR_L | DIR_U
+const EDGE_V = DIR_U | DIR_D
+const EDGE_H = DIR_L | DIR_R
+const END_D = DIR_U
+const END_U = DIR_D
+const END_L = DIR_R
+const END_R = DIR_L
